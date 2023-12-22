d7b8caf910f4e466b3eaa909dc9f0a6e1c1fd1d6
Tarsis <br.tmvdl@gmail.com>
2023-12-22 09:42:40 -0300

    Updating app files

--- src/app/app.py
+++ src/app/app.py
@@ -2,36 +2,38 @@ import flet as ft
 
 def text_button(str, callback):
   def on_click(e):
-    if (str == '<'):
-      callback('Backspace')
-    elif (str == '>'):
-      callback('Enter')
+    if (str == "<"):
+      callback("Backspace")
+    elif (str == ">"):
+      callback("Enter")
     else:
       callback(str)
 
   return ft.TextButton(str, on_click=on_click)
 
 def main(page: ft.Page):
-  page.title = 'Calc app'
-  text_field = ft.Text('', size=100)
+  page.title = "Calc app"
+  page.bgcolor = ft.colors.BLUE_50
+
+  text_field = ft.Text("", size=100)
 
   def write(str):
-    print('write: ' + str)
-    match str.replace('Numpad ', ''):
-      case ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']:
+    print("write: " + str)
+    match str.replace("Numpad ", ""):
+      case ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]:
         text_field.value = text_field.value + str
-      case 'Backspace':
-        print('backspace: ' + text_field.value)
-      case 'Enter':
-        print('enter: ' + text_field.value)
+      case "Backspace":
+        print("backspace: " + text_field.value)
+      case "Enter":
+        print("enter: " + text_field.value)
     page.update()
 
   page.on_keyboard_event = lambda e: write(e.key)
 
   page.add(ft.Row([text_field]))
-  page.add(ft.Row([text_button('7', write), text_button('8', write), text_button('9', write)]))
-  page.add(ft.Row([text_button('4', write), text_button('5', write), text_button('6', write)]))
-  page.add(ft.Row([text_button('1', write), text_button('2', write), text_button('3', write)]))
-  page.add(ft.Row([text_button('<', write), text_button('0', write), text_button('>', write)]))
+  page.add(ft.Row([text_button("7", write), text_button("8", write), text_button("9", write)]))
+  page.add(ft.Row([text_button("4", write), text_button("5", write), text_button("6", write)]))
+  page.add(ft.Row([text_button("1", write), text_button("2", write), text_button("3", write)]))
+  page.add(ft.Row([text_button("<", write), text_button("0", write), text_button(">", write)]))
 
 ft.app(target=main)
