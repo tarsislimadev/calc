73cbd131fddf744a296b4945c539b5fa8bf1c9a6
Tarsis <br.tmvdl@gmail.com>
2023-12-21 21:52:09 -0300

    Updating app files

--- src/app/app.py
+++ src/app/app.py
@@ -1,27 +1,29 @@
 import flet as ft
 
+def text_button(str, callback):
+  def on_click():
+    callback(str)
+
+  return ft.TextButton(str, on_click=on_click)
+
 def main(page: ft.Page):
-  page.title = "Flet counter example"
-  page.vertical_alignment = ft.MainAxisAlignment.CENTER
-  txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+  page.title = "Calc app"
 
-  def minus_click(e):
-    txt_number.value = str(int(txt_number.value) - 1)
+  text_field = ft.TextField(value="", text_align=ft.TextAlign.RIGHT)
+
+  def write(str):
+    text_field.value = text_field.value + str
     page.update()
 
-  def plus_click(e):
-    txt_number.value = str(int(txt_number.value) + 1)
-    page.update()
+  def on_keyboard(e: ft.KeyboardEvent):
+    write(e.key)
 
-  page.add(
-    ft.Row(
-      [
-        ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
-        txt_number,
-        ft.IconButton(ft.icons.ADD, on_click=plus_click),
-      ],
-      alignment=ft.MainAxisAlignment.CENTER,
-    )
-  )
+  page.on_keyboard_event = on_keyboard
+
+  page.add(ft.Row([text_field]))
+  page.add(ft.Row([text_button("7", write), text_button("8", write), text_button("9", write)]))
+  page.add(ft.Row([text_button("4", write), text_button("5", write), text_button("6", write)]))
+  page.add(ft.Row([text_button("1", write), text_button("2", write), text_button("3", write)]))
+  page.add(ft.Row([text_button("<", write), text_button("0", write), text_button(">", write)]))
 
 ft.app(target=main)
