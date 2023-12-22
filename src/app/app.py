2dcb066fea5b5eba7c73f66cbd136dcc1e4ff5e8
Tarsis <br.tmvdl@gmail.com>
2023-12-22 11:43:09 -0300

    Updating app files

--- src/app/app.py
+++ src/app/app.py
@@ -6,10 +6,10 @@ def main(page: ft.Page):
   text_field = ft.Text("", color = ft.colors.BLACK)
 
   def write(text):
-    print("write: " + text)
-    match(str(text).replace("Numpad ", "")):
+    text_string = str(text).replace("Numpad ", "")
+    match(text_string):
       case ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
-        text_field.value = text_field.value + str(text)
+        text_field.value = text_field.value + text_string
       case ["Backspace", "Enter"]:
         text_field.value = ""
     page.update()
